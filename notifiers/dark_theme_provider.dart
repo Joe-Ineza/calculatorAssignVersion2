be1004b67356307b572e1320b45b31c718604982
Joe7Ineza <joe.7neza@gmail.com>
2024-02-20 22:09:31 +0200

    Assignment 3

---
+++ notifiers/dark_theme_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+
+import '../utils/devfestpreferences.dart';
+
+class DarkThemeProvider with ChangeNotifier {
+  DevFestPreferences devFestPreferences = DevFestPreferences();
+  bool _darkTheme = false;
+
+  bool get darkTheme => _darkTheme;
+
+  set darkTheme(bool value) {
+    _darkTheme = value;
+    devFestPreferences.setDarkTheme(value);
+    notifyListeners();
+  }
+}
